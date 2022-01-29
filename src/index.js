c0a522acc48a45d3fd514a8e016d7709ace63993
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-01-28 18:12:34 -0800

    Completed Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  submitTask(form)
 });
+
+function handleNewTask(newTask) {
+  const li = document.createElement('li')
+  const btn = document.createElement('button')
+
+  btn.addEventListener('click', (e) => {
+    e.target.parentNode.remove()
+  })
+  
+  btn.textContent = 'x'
+  li.textContent = `${newTask}  `
+  document.querySelector('ul').appendChild(li).appendChild(btn)  
+}
+
+function submitTask(element) {
+  element.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newTask = event.target['new-task-description'].value
+    handleNewTask(newTask)
+    element.reset()
+  })
+}
